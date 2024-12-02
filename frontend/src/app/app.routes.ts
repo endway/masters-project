1ce1c9d364ae00fd241e17f6eb2b8996654cd8fc
Oleksandr Karpov <okarpov@readdle.com>
2024-12-02 11:59:24 +0200

    feat: Setup routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from "@angular/router";
+import { LoginModule } from "./login";
+import { AuthGuardService } from "./core/auth/auth-guard.service";
+import { UserManagementModule } from "./user-management";
+import { StatusModule } from "./status/status.module";
+import { ProcessSetupModule } from "./process-setup/process-setup.module";
+import { LayoutComponent } from "./shared/components/layout/layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "groups" },
+    { path: "login", pathMatch: "full", loadChildren: () => LoginModule },
+    {
+        path: "",
+        pathMatch: "prefix",
+        canActivate: [AuthGuardService],
+        component: LayoutComponent,
+        children: [
+            { path: "groups", pathMatch: "prefix", canActivate: [AuthGuardService], loadChildren: () => UserManagementModule },
+            { path: "status", pathMatch: "prefix", canActivate: [AuthGuardService], loadChildren: () => StatusModule },
+            { path: "setup", pathMatch: "prefix", canActivate: [AuthGuardService], loadChildren: () => ProcessSetupModule },
+        ],
+    }
+];
